e9c90a12bc580e9c267f3353c8f387079e71a479
dustin <dustin@innoventsoftware.com.au>
2018-05-16 23:39:19 +1000

    Initial Commit

---
+++ Code/WebApiClient/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApiClient.Controllers
+{
+    public class HomeController : ApiController
+    {
+        public HomeController() { }
+
+        [Route("~/api/items")]
+        [HttpGet]
+        public IHttpActionResult Get()
+        { 
+            return Ok();
+        }
+
+        [Authorize]
+        [Route("~/api/items-authorized")]
+        [HttpGet]
+        public IHttpActionResult Get1()
+        {
+            var user = User.Identity.Name;
+            return Ok();
+        }
+
+    }
+}
---
+++ Code/WebApiClient/Startup.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Configuration;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Microsoft.IdentityModel.Logging;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using Microsoft.Owin;
+using Microsoft.Owin.Cors;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.Notifications;
+//using Microsoft.Owin.Security.OpenIdConnect;
+using Owin;
+
+[assembly: OwinStartup(typeof(WebApiClient.Startup))]
+
+namespace WebApiClient
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            HttpConfiguration config = new HttpConfiguration();
+            // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=316888
+            ConfigureAuth(app);
+            ConfigureWebApi(app, config);
+        }
+
+        public void ConfigureAuth(IAppBuilder app)
+        {
+            app.SetDefaultSignInAsAuthenticationType(CookieAuthenticationDefaults.AuthenticationType);
+
+            app.UseCookieAuthentication(new CookieAuthenticationOptions { });
+
+            var baseUrl = ConfigurationManager.AppSettings["IdentityServerBaseUrl"];
+
+            app.UseIdentityServerBearerTokenAuthentication(new IdentityServer3.AccessTokenValidation.IdentityServerBearerTokenAuthenticationOptions
+            {
+                Authority = baseUrl,
+                ClientId = "expedo_client",
+                //RequiredScopes = new[] { "postman_api" }
+            });
+
+            //app.UseOpenIdConnectAuthentication(new OpenIdConnectAuthenticationOptions
+            //{
+            //    ClientId = "expedo_client",
+            //    AuthenticationType = "code",
+            //    Authority = baseUrl,
+            //    Scope = "postman_api",
+            //    RedirectUri = "http://localhost:61744",
+            //    RequireHttpsMetadata = false
+            //});
+
+        }
+
+        private static void ConfigureWebApi(IAppBuilder app, HttpConfiguration config)
+        {
+            WebApiConfig.Register(config);
+            app.UseCors(CorsOptions.AllowAll);
+            app.UseWebApi(config);
+        }
+    }
+
+   
+}
